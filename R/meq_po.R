7516ac323765fd393825b43a3f4f742bc959468f
Rollie Parrish <rparrish@flightweb.com>
2015-05-23 13:00:43 -0700

    Initial documentation for meq_po table

---
+++ R/meq_po.R
@@ -0,0 +1,13 @@
+#' Morphine Equivalent to PO
+#'
+#' Used to convert various opoid dosages
+#' to Morphine equivalent
+#'
+#' @format A data frame with 2 variables:
+#' \describe{
+#'   \item{med_route}{generic name and route}
+#'   \item{meq}{conversion factor to orphine}
+#'   ...
+#' }
+#' @source Needs to be defined
+"meq_po"
